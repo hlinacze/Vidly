c68e6c7af94049c78b5ffc8a551e16b5e7c58bdb
hlinacze <hliba.ondra@live.com>
2017-10-15 12:41:53 +0200

    Přidejte soubory projektu.

---
+++ Vidly/Models/IdentityModels.cs
@@ -0,0 +1,33 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Vidly.Models
+{
+    // Přidáním dalších vlastností do třídy uživatelů můžete přidat data profilu uživatele. Další informace najdete na webu https://go.microsoft.com/fwlink/?LinkID=317594.
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Poznámka: Objekt authenticationType musí odpovídat objektu definovanému objektem CookieAuthenticationOptions.AuthenticationType.
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Zde přidat vlastní deklarace uživatele
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
